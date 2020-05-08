6a5c9b114e0f222b554bef7e86956d8d713dff89
Ankit-adi <ankitaditya02@gmail.com>
2020-05-08 12:08:30 +0530

    Practice Problems

--- src/dhruv.java
+++ src/dhruv.java
@@ -5,7 +5,6 @@ public class dhruv {
                         Math.log(2));
         return p;
     }
-
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
@@ -22,8 +21,7 @@ public class dhruv {
             b[i]=res+k;
         }
         for (int i = 0; i < m; i++) {
-            System.out.println(b[i]);
+            System.out.print(b[i]+" ");
         }
-
     }
 }
